96553a7f78f76fcd8b25c2550da4c776d3b129da
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-01-14 00:07:43 +0600

    Double Rent

---
+++ CodeChef_START_104/Double_Rent.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int rent;cin>>rent;
+    int doubleRent = rent*2;
+    cout<<doubleRent;
+    return 0;
+}
