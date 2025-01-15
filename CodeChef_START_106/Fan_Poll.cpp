2853c07e3dc919e06e8418cd54d0fbabfd010b9e
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-01-16 01:59:20 +0600

    fan Poll

---
+++ CodeChef_START_106/Fan_Poll.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<(a>b&&a>c?"Yes":"No")<<endl;
+    return 0;
+}
