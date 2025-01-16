9f244c2f24c01a56da8bdf77bd2df568c6585a81
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-01-17 01:51:19 +0600

    Clear Day

---
+++ CodeChef_START_107/Clear_Day.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int x,y;
+    cin>>x>>y;
+    cout<<(7-(x+y));
+    return 0;
+}
