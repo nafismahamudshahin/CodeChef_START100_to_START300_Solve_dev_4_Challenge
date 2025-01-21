853b0c73585b5235a2d46b830d7f56204d2713c2
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-01-21 16:13:49 +0600

    404 not found

---
+++ CodeChef_START_111/404_Not_Found.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int response;cin>>response;
+    cout<<(response==404?"NOT FOUND":"FOUND");
+    return 0;
+}
