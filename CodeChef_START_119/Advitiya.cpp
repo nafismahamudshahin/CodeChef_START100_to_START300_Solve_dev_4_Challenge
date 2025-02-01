b703596ab78cbb7d40aa7fd64a66c163090ed3b7
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-02-01 23:07:03 +0600

    Advitiya

---
+++ CodeChef_START_119/Advitiya.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int d;cin>>d;
+    if(d>=16 && d<=18) cout<<"ADVITIYA";
+    else cout<<"WAITING FOR ADVITIYA";
+    return 0;
+}
