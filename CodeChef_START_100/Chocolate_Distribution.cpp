15b0df33270d77fad33838bdd1be0e911ac0f996
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-02-17 23:48:30 +0600

    update code

--- CodeChef_START_100/Chocolate_Distribution.cpp
+++ CodeChef_START_100/Chocolate_Distribution.cpp
@@ -8,8 +8,9 @@ int main(){
     fast;
     int t;cin>>t;
     while(t--){
-        int n;cin>>n;
+        int n;
+        cin>>n;
         cout<<1<<" "<<1<<" "<<n-2<<endl;
-    }
+    } 
     return 0;
 }
