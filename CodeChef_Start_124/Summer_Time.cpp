de60867698cd49d57e65e7ae374729c62f0a476b
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-03-25 22:49:58 +0600

    Summer time

---
+++ CodeChef_Start_124/Summer_Time.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int x;cin>>x;
+    cout<<(x>35?"YES":"NO");
+    return 0;
+}
