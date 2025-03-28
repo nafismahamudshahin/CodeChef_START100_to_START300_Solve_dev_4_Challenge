cdf4f9c778a0aa9c17cc434056dcefe7b3e6f0e4
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-03-28 23:48:20 +0600

    50 50 Rule

---
+++ CodeChef_Start_125/50_50_Rule.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int t;cin>>t;
+    while(t--){
+        int attendense,mark;
+        cin>>attendense>>mark;
+        if(attendense<50) cout<<"Z"<<endl;
+        else if(mark<50 && attendense>=50) cout<<"F"<<endl;
+        else cout<<"A"<<endl;
+    }
+    return 0;
+}
